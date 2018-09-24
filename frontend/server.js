78a07f04be7a47591740c87b114f6b714c187eb5
Connor G Meehan <connorgmeehan23@gmail.com>
2018-09-25 00:00:45 +1000

    Server takes port from ENV variables

--- frontend/server.js
+++ frontend/server.js
@@ -5,7 +5,7 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
-const port = process.env.PORT;
+const port = ( process.env.PORT ? process.env.PORT : 3000 );
 
 app.prepare()
 .then(() => {
